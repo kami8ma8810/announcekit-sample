4e1608dda451b18f51b8938bc212af5bf58f5173
Hayato Kamiyama <60092195+kami8ma8810@users.noreply.github.com>
2025-02-12 20:07:42 +0900

    feat: add AnnounceKit integration with button trigger in HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+import type { AnnouncekitInstance } from 'announcekit-vue'
+
+const ankRef = ref<AnnouncekitInstance>()
+
+const handleClick = () => ankRef.value?.open()
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <button @click="handleClick" type="button">What's New</button>
+    <AnnounceKit ref="ankRef" widget="https://announcekit.co/widgets/v2/4jlW8w" />
+    <!-- <div class="col-md-10">
+
+      <div class="form-group">
+        <h2>Line widget</h2>
+        It inherits parent style
+        <br />
+        <AnnounceKit catchClick=".ak-trigger" widget="https://announcekit.app/widgets/v2/35ULgQ" />
+      </div>
+
+      <AnnounceKit
+        :float-widget="true"
+        :widget-style="{ bottom: '100px' }"
+        widget="https://announcekit.app/widgets/v2/q9imc"
+      />
+    </div> -->
   </main>
 </template>
